587d1b95ff6a97ec5eb4b500f0c85b542b2d5d67
jatin <jagdish@kunato.io>
2024-05-09 17:27:57 +0530

    implement echo command

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{io::{prelude::*, BufReader}, net::{TcpListener, TcpStream}, thread};
+use std::str::from_utf8;
+use std::{
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+    thread,
+};
 // Uncomment this block to pass the first stage
 
 fn main() {
@@ -11,8 +16,8 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok( stream) => {
-                thread::spawn(||{
+            Ok(stream) => {
+                thread::spawn(|| {
                     handle_client(stream);
                 });
             }
@@ -23,33 +28,52 @@ fn main() {
     }
 }
 
-fn handle_client(mut stream:TcpStream){
-    let mut buffer = [0;1024];
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
 
     loop {
         let read_count = stream.read(&mut buffer).unwrap();
+        let mut text = from_utf8(&buffer).unwrap();
+
         if read_count == 0 {
             break;
         }
 
-        stream.write_all("+PONG\r\n".as_bytes());
+        let mut lines = text.lines();
+        let mut commands: Vec<String> = Vec::new();
+
+        while let Some(line) = lines.next() {
+            if line.starts_with("*") {
+                // TODO
+            } else if line.starts_with("$") {
+                if let Some(data) = lines.next() {
+                    commands.push(data.to_string());
+                }
+            }
+        }
+
+        let command = commands.get(0).unwrap();
+        let output = commands
+            .get(1)
+            .map(|s| s.to_string()) // Convert &String to String if there is Some
+            .unwrap_or_else(|| "".to_string());
+
+        let command_upper = command.to_uppercase();
+
+        match command_upper.as_str() {
+            "PING" => {
+                println!("PONG");
+                let response = "+PONG\r\n";
+                stream.write_all(response.as_bytes()).unwrap();
+            }
+            "ECHO" => {
+                println!("output {}", output);
+                let response = format!("${}\r\n{}\r\n", output.len(), output);
+                stream.write_all(response.as_bytes()).unwrap();
+            }
+            _ => {
+                println!("Something else is found ");
+            }
+        }
     }
 }
-
-// let buf_reader = BufReader::new(&mut stream);
-
-// for line_result in buf_reader.lines() {
-//     let line = line_result.unwrap();
-//
-//     println!("line {}", line);
-//     if line == "PING"{
-//         let response = "+PONG\r\n";
-//         println!("PING is received");
-//         //
-//         continue;
-//     }
-//     else{
-//         // break;
-//         continue;
-//     }
-// }
